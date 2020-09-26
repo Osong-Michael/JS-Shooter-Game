4857d10495e345526021be80a65da898b7abc633
Osong Agberndifor <agberndifor7@gmail.com>
2020-09-26 22:04:06 +0100

    Setup project with linter configs and webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav|mpe?g)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  watch: true,
+};
